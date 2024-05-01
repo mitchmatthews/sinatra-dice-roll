01904aef91ee6d23f9730b3c5f6248ba8f14b9d7
Mitch Matthews <165244888+mitchmatthews@users.noreply.github.com>
2024-05-01 23:33:35 +0000

    Wrapper and Views added

--- lucky_number.rb
+++ lucky_number.rb
@@ -14,8 +14,6 @@ get("/howdy") do
 end
 
 get("/") do
-  lucky_num = rand(1..6)
-  return "You lucky number for today is #{lucky_num}"  
   erb(:elephant)
 end
 
@@ -23,15 +21,16 @@ get("/giraffe") do
   "Hopefully this shows up without having to restart the server again 🤞🏾"
 end
 
+
+
 get("/dice/2/6") do
   first_die = rand(1..6)
-	second_die = rand(1..6)
+  second_die = rand(1..6)
   sum = first_die + second_die
-	
-	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-	"<h1>2d6</h1>
-   <p>#{outcome}</p>"
+
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  erb(:two_six, { :layout => :wrapper })
 end
 
 get("/dice/2/10") do
@@ -39,19 +38,17 @@ get("/dice/2/10") do
 	second_die = rand(1..10)
   sum = first_die + second_die
 	
-	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-	"<h1>2d10</h1>
-   <p>#{outcome}</p>"
+	erb(:two_ten, { :layout => :wrapper })
 end
 
 get("/dice/1/20") do
   die = rand(1..20)
 	
-	outcome = "You rolled a #{die}."
+	@outcome = "You rolled a #{die}."
 	
-	"<h1>1d20</h1>
-   <p>#{outcome}</p>"
+	erb(:one_twenty, { :layout => :wrapper })
 end
 
 get("/dice/5/4") do
@@ -61,8 +58,7 @@ get("/dice/5/4") do
   fourth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die
 	
-	outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
 	
-	"<h1>5d4</h1>
-   <p>#{outcome}</p>"
+	erb(:two_ten, { :layout => :wrapper })five_four
 end
