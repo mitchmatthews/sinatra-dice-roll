22ca9ce1ad34e0dccd268f6988fc086345d5cf41
Mitch Matthews <165244888+mitchmatthews@users.noreply.github.com>
2024-05-02 00:12:13 +0000

    added 100_6

--- dice.rb
+++ dice.rb
@@ -44,9 +44,9 @@ get("/dice/2/10") do
 end
 
 get("/dice/1/20") do
-  die = rand(1..20)
+  @die = rand(1..20)
 	
-	@outcome = "You rolled a #{die}."
+	@outcome = "You rolled a #{@die}."
 	
 	erb(:one_twenty, { :layout => :wrapper })
 end
@@ -56,9 +56,22 @@ get("/dice/5/4") do
 	second_die = rand(1..4)
   third_die = rand(1..4)
   fourth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
 	
-	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
 	
 	erb(:five_four, { :layout => :wrapper })
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
